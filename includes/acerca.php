0fb62bde80338c2922bebb11536615a009b5d7ab
aibanezm-esliceu <aibanezm@esliceu.net>
2026-04-16 20:52:55 +0200

    acerca creado

---
+++ includes/acerca.php
@@ -0,0 +1,12 @@
+<?php 
+// Cambiamos el título para esta página
+$pageTitle = "Acerca de - Examen PHP"; 
+require __DIR__ . "/includes/header.php"; 
+?>
+
+<h1>Acerca de este proyecto</h1>
+<p>Esta es la página de información. Todo está funcionando correctamente usando includes de PHP.</p>
+
+<?php 
+require __DIR__ . "/includes/footer.php"; 
+?>
