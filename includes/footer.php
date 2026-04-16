61ca0368b5939973cf90d80eb7daf3081b6fa8e0
aibanezm-esliceu <aibanezm@esliceu.net>
2026-04-16 20:43:01 +0200

    Añadiendo includes y el index

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+</main> <footer>
+        <p>&copy; 2026 - Proyecto Examen PHP. Creado por Alex.</p>
+    </footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo isset($pageTitle) ? $pagTitle : 'Examen PHP Alejandro Ibañez' ?></title>
+    <link rel="stylesheet" href="/css/style.css?v=>?php echo time(); ?:">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a><>/li>
+            <li><a href="acerca de mi.php">Acerca de mi</a><>/li>
+            <li><a href="galeria.php">Galería</a><>/li>
+            <li><a href="listado.php">Listad BD>/a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+</ul>
+</nav>
+
+    
+</body>
+</html>
